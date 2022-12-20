4946cd8ee4d180828469be07dc84d515c37661a1
jannatulmawamahi <115892307+jannatulmawamahi@users.noreply.github.com>
2022-12-20 21:53:24 +0600

    Add files via upload

---
+++ js/apps.js
@@ -0,0 +1,11 @@
+
+
+$(function () {
+    $('.banner_slider').slick({
+        arrows: false,
+        autoplay: true,
+        autoplayspeed: 600,
+        speed:600,
+        dots:true,
+    })
+  });
